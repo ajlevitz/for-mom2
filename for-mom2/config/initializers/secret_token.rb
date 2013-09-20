c88f25d45d2dc7461e12daa14259d8418c6021bb
Alyssa Levitz <alyssal@yammer-inc.com>
2013-09-19 22:46:18 -0700

    new rails app

---
+++ for-mom2/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ForMom2::Application.config.secret_key_base = '0a7ddfbac321cbd1454e5a628502b6dc74eb84f4ed3cb78b95a4dd797be0e59e38c6e5220b609d4b586495e8a41daf3dbb716a9b77da08da05cd4fda1a68efc8'
